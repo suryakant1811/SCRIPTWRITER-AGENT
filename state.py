f481b8cc939f73bfcfb45723d25ed82915e83ff2
suryadwd <surajdwivedi644@gmail.com>
2026-07-13 16:43:07 +0530

    added state file

--- state.py
+++ state.py
@@ -1,7 +1,7 @@
-from langgraph.graph import messageState
+from langgraph.graph import MessagesState
 
-class state(MessageState):
+class ContentState(MessagesState):
     raw_script: str
-    editor_script: str
-    format_script: str
+    edited_script: str
+    formatted_script: str
     hinglish_script: str
